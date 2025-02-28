7f1d79cc57ff2e3519b36ef318c81df542a83747
mvsanghvi <mevsanghvi@gmail.com>
2025-02-27 23:19:58 -0600

    Solved Practice

---
+++ Week 1/1.2.2.py
@@ -0,0 +1,9 @@
+#Count the number of 3 nucleotide sequences in a given string
+
+def PatternCount(Text, Pattern):
+    # fill in your function here
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1
+    return count 
