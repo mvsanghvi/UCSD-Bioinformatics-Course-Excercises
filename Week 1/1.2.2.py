9113e7415f43819a18cb7196ab0890de440eed38
mvsanghvi <mevsanghvi@gmail.com>
2025-03-01 16:28:33 -0600

    Simplify Solution

--- Week 1/1.2.2.py
+++ Week 1/1.2.2.py
@@ -5,5 +5,5 @@ def PatternCount(Text, Pattern):
     count = 0
     for i in range(len(Text)-len(Pattern)+1):
         if Text[i:i+len(Pattern)] == Pattern:
-            count = count+1
+            count += 1
     return count 
